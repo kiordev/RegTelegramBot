1e0d7bf070e0f9a164079965128979b0c6a090e7
kiordev <kiordev@gmail.com>
2022-04-18 21:38:45 +0300

    Bot Ready To Use

--- main.py
+++ main.py
@@ -12,33 +12,22 @@ client = telebot.TeleBot(Configuration.config['token'])
 
 
 # Main Code:
-
 @client.message_handler(commands=['start'])
-def bot_start(message):
-    msg = client.send_message(message.chat.id, "Начало работы")
-    client.register_next_step_handler(msg, application)
+def start_message(message):
+    client.send_message(message.chat.id, "Menu: /info - информация про вас")
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    keyboard.add(types.KeyboardButton("Info"))
+    msg = client.send_message(message.chat.id, "Выберите пункт", reply_markup=keyboard)
+    client.register_next_step_handler(msg, main_work)
 
-def application(message):
-    rmk = types.ReplyKeyboardMarkup(resize_keyboard=True)  # Создание клавиатуры
-    rmk.add(types.KeyboardButton("Да"), types.KeyboardButton("No"))  # Создание двух кнопок
-    msg = client.reply_to(message, "Желаете подать заявку на модератора?", reply_markup=rmk)  # Добавление клавиатуры
-    client.register_next_step_handler(msg, user_answer)  # Переводчик на следующий шаг в функцию user_answer
-
-
-def user_answer(message):
-    if message.text == "Да":
-        msg = client.reply_to(message, "Впишите ваши данные: ")
-        client.register_next_step_handler(msg, user_reg)
-    elif message.text == "Нет":
-        client.message_handler(message.chat.id, "Ну и пока!")
+def main_work (message):
+    if message.text == "Info":
+        client.send_message(message.chat.id, "<b>Информация про вас:</b> ", parse_mode='html')
+        client.send_message(message.chat.id, f" Имя: {message.from_user.first_name}, Фамилия: {message.from_user.last_name}")
     else:
-        client.message_handler(message.chat.id, "Прочитай, что ты ввел")
+        client.send_message(message.chat.id, "Иди нахуй")
 
 
-def user_reg(message):
-    client.send_message(message.chat.id, f"Your date: {message.text}")
-
-
-# Telegram Bot works void
 client.enable_save_next_step_handlers(delay=2)
+client.load_next_step_handlers()
 client.infinity_polling()
--- test.py
+++ test.py
@@ -1,3 +1,8 @@
+# Testing Regestration Bot by Python Telebot
+# Creator: kiordev@gmail.com | with youtuber "Фсоки"
+# Date: 17 APRIL 2022
+
+# imports:
 import telebot
 from telebot import types
 import Configuration
@@ -6,21 +11,30 @@ import Configuration
 client = telebot.TeleBot(Configuration.config['token'])
 
 
-@client.message_handler(commands=['help', 'start'])
+# Main Code:
 
-def send_welcome(message):
-    msg = client.send_message(message.chat.id, """\
-Hi there, I am Example bot.
-What's your name?
-""")
-    client.register_next_step_handler(msg, process_name_step)
+@client.message_handler(commands=['start'])
+def bot_start(message):
+    rmk = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)  # Создание клавиатуры
+    rmk.add(types.KeyboardButton("Да"), types.KeyboardButton("No"))  # Создание двух кнопок
+    msg = client.send_message(message.chat.id, "Желаете подать заявку на модератора?", reply_markup=rmk)  # Добавление клавиатуры
+    client.register_next_step_handler(msg, user_answer)  # Переводчик на следующий шаг в функцию user_answer
+
+def user_answer(message):
+    if message.text == "Да":
+        msg = client.reply_to(message, "Впишите ваши данные: ")
+        client.register_next_step_handler(msg, user_reg)
+    elif message.text == "Нет":
+        client.message_handler(message.chat.id, "Ну и пока!")
+    else:
+        client.message_handler(message.chat.id, "Прочитай, что ты ввел")
 
 
-def process_name_step(message):
-    client.send_message(message.chat.id, "Working")
+def user_reg(message):
+    client.send_message(message.chat.id, f"Your date: {message.text}")
 
 
-
+# Telegram Bot works void
 client.enable_save_next_step_handlers(delay=2)
 client.load_next_step_handlers()
 client.infinity_polling()
